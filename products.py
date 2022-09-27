ee3f0154098c00892fccfa8a63098c7db1b245c7
Cheryl Hsieh <ch84y1213y@gmail.com>
2022-09-27 21:18:07 +0800

    add header

--- products.py
+++ products.py
@@ -11,6 +11,13 @@ while True:
 print(products)	
 #7~9行可以簡略成 p = [name, price]，或第10行直接簡略成products.append([name, price])
 
+with open('products.csv', 'w', encoding='utf-8') as f: #w為寫入模式，本行僅為打開檔案
+	#encoding為編碼，為解決亂碼問題，utf-8最廣泛使用之編碼
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') #本行才是做真正寫入
+
 #存取二維清單
 products[0][0] #將第0個商品的name拿出來
 products[1][1] #將第1個商品的price拿出來
+
