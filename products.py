9d5aabfc013e0d995f37fb3f6978e5c19aa8ae0b
Cheryl Hsieh <ch84y1213y@gmail.com>
2022-10-01 13:39:57 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f: #寫入檔案時有用編碼，讀取則須用同種編碼，才不會讀取異常
+	for line in f:
+		if '商品, 價格' in line:
+			continue #遇到'商品,價格'則7-8行跳過，直接跳到下一迴，未逃出迴圈
+		name, price = line.strip().split(',') #用.strip()來除掉換行符號(\n)、用.split(',')來用逗點做切割
+		products.append([name, price])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q': #quit離開
@@ -11,9 +21,14 @@ while True:
 print(products)	
 #7~9行可以簡略成 p = [name, price]，或第10行直接簡略成products.append([name, price])
 
+#印出所有購買記錄
+for p in products:
+	print(p[0], '的價格是', p[1])
+
+#寫入檔案	
 with open('products.csv', 'w', encoding='utf-8') as f: #w為寫入模式，本行僅為打開檔案
 	#encoding為編碼，為解決亂碼問題，utf-8最廣泛使用之編碼
-	f.write('商品, 價格\n')
+	f.write('商品, 價格\n') #加標題
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n') #本行才是做真正寫入
 
